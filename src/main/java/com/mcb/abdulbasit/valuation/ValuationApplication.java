79735aea2244c9200d525f6540bce072633674da
Abdul-Basit.O.A <adebayoabdulbasit@gmail.com>
2024-05-30 15:56:05 +0100

    initial commit - project structure

---
+++ src/main/java/com/mcb/abdulbasit/valuation/ValuationApplication.java
@@ -0,0 +1,13 @@
+package com.mcb.abdulbasit.valuation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValuationApplication.class, args);
+	}
+
+}
